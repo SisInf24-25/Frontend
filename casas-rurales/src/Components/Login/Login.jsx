0839d22d270151319175b4bb4cf62a76eaf0ed7e
manu <158587137+manu839304@users.noreply.github.com>
2024-10-02 22:22:48 +0200

    pantalla inicio sesion/registro creada, falta implementar botones, input, etc

--- casas-rurales/src/Components/Login/Login.jsx
+++ casas-rurales/src/Components/Login/Login.jsx
@@ -1,29 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import icon_user from '../Assets/person.png'
 import icon_password from '../Assets/password.png'
+import icon_email from '../Assets/email.png'
 
 const LoginSignup = () => {
+
+  const [action,setAction] = useState("Iniciar Sesión");
+
+
+
+
+
   return (
     <div className='container'>
       <div className='header'>
-        <div className='text'>Iniciar Sesión</div>
+        <div className='text'>{action}</div>
         <div className='underline'></div>
       </div>
       <div className='inputs'>
-
+      
         <div className="input">
             <img src={icon_user} alt="" />
             <input type="text" placeholder="Nombre de usuario"/>
         </div>
+
+        {action==="Iniciar Sesión"?<div></div>:<div className="input">
+            <img src={icon_email} alt="" />
+            <input type="text" placeholder="Correo Electrónico"/>
+        </div>}
 
         <div className="input">
             <img src={icon_password} alt="" />
             <input type="password" placeholder="Contraseña"/>
         </div>
         <div className="submit-container">
-            <div className="submit">Iniciar Sesión</div>
-            <div className="submit">Registrarse</div>
+            <div className={action==="Iniciar Sesión"?"submit":"submit gray"}onClick={()=>{setAction("Iniciar Sesión")}}>Iniciar Sesión</div>
+            <div className={action==="Registrarse"?"submit":"submit gray"}onClick={()=>{setAction("Registrarse")}}>Registrarse</div>
         </div>
       </div>
     </div>
