fe879dc249fe51ccb722b1fe1640f04b2cbd8329
Alejandro Lopez <845154@unizar.es>
2024-10-04 12:41:43 +0200

    Múltiples páginas

--- casas-rurales/src/App.js
+++ casas-rurales/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login/Login';
+import Index from './Components/Index/Index';
 
 function App() {
   return (
-    <div>
-      <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/index" element={<Index />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ casas-rurales/src/Components/Index/Index.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './Index.css'
+import icon_user from '../Assets/person.png'
+import icon_password from '../Assets/password.png'
+import icon_email from '../Assets/email.png'
+
+const LoginSignup = () => {
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>Index</div>
+        <div className='underline'></div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginSignup
