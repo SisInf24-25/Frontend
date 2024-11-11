4f214ec030b2994a38f6a2941aa881fdad99f5d2
Manu (839304) <839304@unizar.es>
2024-11-11 10:52:33 +0100

    authprovider y protectedroute metidos, pero sin usar || pagina Auth ahora contiene los campos corrector, y comprueba que las passwords coinciden

---
+++ casas-rurales/src/Context/AuthProvider.jsx
@@ -0,0 +1,67 @@
+import { createContext, useState, useEffect } from "react";
+import io from 'socket.io-client';
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useState(() => {
+        // Inicializar el estado 'auth' con el valor guardado en el localStorage si existe,
+        // de lo contrario, inicializarlo como un objeto vacío.
+        const savedAuth = localStorage.getItem('auth');
+        return savedAuth ? JSON.parse(savedAuth) : {};
+    });
+
+    const [socket, setSocket] = useState(null);
+    //const [refreshFriends, setRefreshFriends] = useState(false);
+    /*
+    useEffect(() => {
+        console.log('Entra al useEffect')
+        if (auth.username && !socket) {
+            // Establecer la conexión con el servidor de Socket.io
+            console.log('Entra al if')
+            const newSocket = io("https://server.narratives.es", {
+                withCredentials: true,
+            });
+            console.log(newSocket);
+            setSocket(newSocket);
+        }
+        else if (!auth.username && socket) {
+            socket.disconnect();
+        }
+    }, [auth]);
+    */
+    // Almacenar la sesión en el almacenamiento local al cambiar
+    useEffect(() => {
+        localStorage.setItem('auth', JSON.stringify(auth));
+    }, [auth]);
+
+    // Comprobar si hay una sesión almacenada en el almacenamiento local al cargar la página
+    useEffect(() => {
+        const savedAuth = localStorage.getItem('auth');
+        if (savedAuth) {
+            setAuth(JSON.parse(savedAuth));
+        }
+    }, []);
+
+    const updateUsername = (newUsername) => {
+        setAuth({
+            ...auth,
+            username: newUsername
+        });
+    }
+
+    const updateMail = (newEmail) => {
+        setAuth({
+            ...auth,
+            correo: newEmail
+        });
+    }
+
+    return(
+        <AuthContext.Provider value={{ auth, setAuth, socket, refreshFriends, setRefreshFriends, updateUsername, updateMail}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
--- casas-rurales/src/Pages/Auth.jsx
+++ casas-rurales/src/Pages/Auth.jsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import icon_user from '../Assets/person.png'
-import icon_password from '../Assets/password.png'
-import icon_email from '../Assets/email.png'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -17,9 +14,12 @@ const Auth = () => {
 
   const [action, setAction] = useState(initialAction);
   const [userType, setUserType] = useState("guest");
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
   
-  const notify = () => toast.error("ERROR MESSAGE!!!", {
+  const notify = () => toast.error("Las contraseñas deben coincidir", {
     position: "bottom-left",
     autoClose: 5000,
     hideProgressBar: false,
@@ -28,6 +28,46 @@ const Auth = () => {
     draggable: true,
     progress: undefined
   });
+
+
+
+  const handleAcceptButtonClick = () => {
+    if (action === "login") {
+      handleLoginAcceptButtonClick()
+    } /*signup*/else if (passwordError) {
+      notify()
+    } else {
+      handleSignupAcceptButtonClick()
+    }
+  };
+
+  const handleLoginAcceptButtonClick = () => {
+    // TODO: CAMBIARLO POR LA PETICIÓN
+    console.log("login")
+  };
+
+  const handleSignupAcceptButtonClick = () => {
+    // TODO: CAMBIARLO POR LA PETICIÓN
+    console.log("signup - ", userType)
+  };
+
+
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    const value = e.target.value;
+    setConfirmPassword(value);
+
+    // Validar que las contraseñas coincidan
+    if (value !== password) {
+      setPasswordError('Las contraseñas no coinciden');
+    } else {
+      setPasswordError('');
+    }
+  };
   
 
   useEffect(() => {
@@ -59,19 +99,35 @@ const Auth = () => {
         
       <div className="inputs">
         <div className="input">
-          <img src={icon_user} alt="User" />
           <input type="text" placeholder="Nombre de usuario" />
         </div>
 
         { action === "login" ? <div></div>:<div className="input">
-          <img src={icon_email} alt="Email" />
-          <input type="text" placeholder="Correo Electrónico" />
+          <input type="text" placeholder="Nombre"/>
+        </div> }
+
+        { action === "login" ? <div></div>:<div className="input">
+          <input type="text" placeholder="Apellidos"/>
+        </div> }
+
+        { action === "login" ? <div></div>:<div className="input">
+          <input type="email" placeholder="Correo electrónico"/>
+        </div> }
+
+        { action === "login" ? <div></div>:<div className="input">
+          <input type="tel" placeholder="Número de teléfono"/>
         </div> }
 
         <div className="input">
-          <img src={icon_password} alt="Password" />
-          <input type="password" placeholder="Contraseña" />
+          <input type="password" placeholder="Contraseña" value={password} onChange={handlePasswordChange}/>
         </div>
+
+        { action === "login" ? <div></div>:<div className="input">
+          <input type="password" placeholder="Repetir contraseña" value={confirmPassword} onChange={handleConfirmPasswordChange}/>
+        </div> }
+
+        {passwordError && <p id="mensaje-error-contraseñas" style={{ color: 'red' }}>{passwordError}</p>} {/* Mostrar mensaje de error si las contraseñas no coinciden */}
+        
 
         { action === "login" ? <div></div>:<div className="user-type-container">
             <div className={ userType === "guest" ? "user-type" : "user-type gray" } onClick={ () => { setUserType("guest") } }>Huésped</div>
@@ -79,7 +135,7 @@ const Auth = () => {
         </div> }
 
         <div className="accept">
-          <div id="auth" className="accept-button" onClick={ () => { console.log(`${action} - ${userType}`); notify() }}>{actionNames[action]}</div>
+          <div id="auth" className="accept-button" onClick={ () => { handleAcceptButtonClick() }}>{actionNames[action]}</div>
         </div>
         <ToastContainer/>
       </div>
